154a4b7b2c6bc17ed8e83f0e263238b061477db1
jjcodelogics <jjcodelogics@gmail.com>
2025-12-11 23:28:40 +0100

    aboutme page

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
         trailer: path.resolve(__dirname, 'trailer.html'),
+        about: path.resolve(__dirname, 'about-me.html'),
         contact: path.resolve(__dirname, 'contact.html')
       }
     }
